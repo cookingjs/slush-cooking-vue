d508adbe9e7e3b07c3a852a089dc43c979920602
qingwei.li <cinwell.li@gmail.com>
2016-06-10 22:38:48 +0800

    update array chunk

--- template/cooking.conf.js
+++ template/cooking.conf.js
@@ -19,8 +19,9 @@ cooking.set({
   clean: true,
   hash: true,
   sourceMap: true,
-  chunk: {
-    'vendor': {
+  chunk: [
+    {
+      name: 'vendor',
       minChunks: function (module, count) {
         // any required modules inside node_modules are extracted to vendor
         return (
@@ -32,12 +33,13 @@ cooking.set({
         );
       }
     },
-    // extract webpack runtime and module manifest to its own file in order to
-    // prevent vendor hash from being updated whenever app bundle is updated
-    'manifest': {
+    {
+      // extract webpack runtime and module manifest to its own file in order to
+      // prevent vendor hash from being updated whenever app bundle is updated
+      name: 'manifest',
       chunks: ['vendor']
     }
-  },
+  ],
   publicPath: '/dist/',
   assetsPath: 'static',
   urlLoaderLimit: 10000,
